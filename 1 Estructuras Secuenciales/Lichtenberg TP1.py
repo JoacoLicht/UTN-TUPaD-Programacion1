8a9647b3f02c04da7aa983ced301b660297aabc2
Joaquin Lichtenberg <joaquinlichtenberg9@gmail.com>
2026-08-22 20:12:31 -0300

    Creamos la primera carpeta
    que contiene todos los ejercicios de la unidad anterior

---
+++ 1 Estructuras Secuenciales/Lichtenberg TP1.py
@@ -0,0 +1,59 @@
+#Ejercicio 1 
+# print("Hola Mundo!")
+
+#Ejercicio 2
+#nombre = input("Ingrese su nombre ")
+#print(f"Hola {nombre}!")
+
+#Ejercicio 3
+#nombre = input("Ingrese su nombre: ")
+#apellido= input("Ingrese su apellido: ")
+#edad = int(input("Ingrese su edad: "))
+#lugarResidencia = input("Lugar de residencia :")
+
+#print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {lugarResidencia}.") 
+
+#Ejercicio 4
+#radio =float(input("Ingrese el radio: "))
+#area= 3.14 * radio**2
+#perimetro = 2* 3.14 * radio
+#print(f"El area es: {area} y el perimetro es: {perimetro}")
+
+#Ejercicio 5
+#segundos = int(input(" Ingrese los segundos: "))
+#hora = segundos / 3600
+#print(f" los segundos equivalen a {hora} horas")
+
+#Ejercicio 6
+#numero =int(input("Dime un numero "))
+#for  i in range(1, 11):
+#    print(f" {numero} x {i} = {numero * i}" )
+
+#Ejercicio 7
+#while True:
+#    numero1=int(input("Ingrese el primer numero: "))
+#    numero2=int(input("Ingrese el segundo numero: "))
+#    if numero1 == 0 or numero2 == 0:
+#        print("ingrese de vuelta los numeros")
+#    else:
+#        print(f"suma: {numero1 + numero2} division: {numero1/ numero2} multiplicacion: {numero1 * numero2} resta: {numero1 - numero2}")
+#        break
+
+#Ejercicio 8
+#altura = float(input("Ingrese su altura: "))
+#peso = float(input("Ingrese su peso: "))
+#imc= peso / altura**2
+#print(f"su indice de masa corporal es: {imc}")
+
+#Ejercicio 9
+#cel = int(input("Ingrese la temperatura de grados celsius: "))
+#fahr= 9/5* cel + 32
+#print(f"En grados Fahrenheit equivale a: {fahr}")
+
+#Ejercicio 10
+#numero1= int(input("Ingrese el primer numero: "))
+#numero2= int(input("Ingrese el segundo numero: "))
+#numero3= int(input("Ingrese el tercer numero: "))
+
+#prom= (numero1 + numero2 + numero3) /3
+#print(f"Tu promedio es: {prom}")
